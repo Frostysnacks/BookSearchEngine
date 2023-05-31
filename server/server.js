ae656684950e900a109a349ec076738cdcd06aff
Frostysnacks <rbernier93@gmail.com>
2023-05-31 15:20:23 -0400

    heroku

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const db = require("./config/connection");
-
+const mongoose = require('mongoose')
 // add apollo server
 const { ApolloServer } = require("apollo-server-express");
 const { typeDefs, resolvers } = require("./schemas");
